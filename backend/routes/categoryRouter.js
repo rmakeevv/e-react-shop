beecaead5d44e35343d8bbf1e84bf30a6f31f332
almakeev <r.makeevv@gmail.com>
2024-06-15 16:01:44 +0300

    update

--- backend/routes/categoryRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const {getCategory} = require("../controllers/categoryController");
-const categoryRouter = express.Router()
-
-categoryRouter.get('/:category/:order', getCategory)
-
-module.exports = categoryRouter
