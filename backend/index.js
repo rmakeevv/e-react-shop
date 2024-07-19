1b34209f20ae320be007a768e8c668d566280442
almakeev <r.makeevv@gmail.com>
2024-07-20 02:21:12 +0300

    .editorconfig

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {client} = require('./db')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
-const cors = require('cors')
-require('dotenv').config()
+const { client } = require("./db");
+const bodyParser = require("body-parser");
+const router = require("./routes/router");
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/', router)
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/", router);
+
+app.use("/");
+
+app.use(express.static("public"));
 async function start() {
-    await client.connect()
-        .then(() => console.log('db connected'))
-        .catch(e => console.dir(e))
-    app.listen(5001)
+    await client
+        .connect()
+        .then(() => console.log("db connected"))
+        .catch((e) => console.dir(e));
+    app.listen(5001);
 }
-start().then(() => console.log('server is running')).catch(e => console.log(e.message))
+start()
+    .then(() => console.log("server is running"))
+    .catch((e) => console.log(e.message));
